a1441cfc58fa82f544c49b7a6aab74d095b3b559
vigilantalphagroup <vigilantalphagroup@gmail.com>
2026-06-22 15:53:55 -0400

    Delete Simgabond directory
    fresh delete

--- Simgabond/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SigmaBondCoPilot from './SigmaBondCoPilot.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SigmaBondCoPilot />
-  </React.StrictMode>,
-)
